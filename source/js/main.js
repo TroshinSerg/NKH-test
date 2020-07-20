af1c447b20fe7986697a6b15e69da0b04a03a1b4
TroshinSerg <serge-troshin@yandex.ru>
2020-07-20 11:54:31 +0300

    Добавляет блок в соответствии с пунктами #8, #9 ТЗ

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 document.querySelector('html').classList.remove('no-js');
+var accButtons = document.querySelectorAll('.accordion__item-btn');
+
+var ActiveClass = {
+  ACC_BTN: 'accordion__item-btn--active',
+  ACC_TXT: 'accordion__item-text--showed'
+};
+
+accButtons.forEach(function(btn) {
+  btn.addEventListener('click', onAccBtnClick);
+});
+
+function onAccBtnClick(evt) {
+  var accBtn = evt.currentTarget;
+  var accText = accBtn.nextElementSibling;
+  console.log(accText);
+
+  accBtn.classList.toggle(ActiveClass.ACC_BTN);
+  accText.classList.toggle(ActiveClass.ACC_TXT);
+}
